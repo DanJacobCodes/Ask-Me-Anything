1ddd45a75af497ff155c216b8204596ce9456b23
Dan Jacob <dansamueljacob@gmail.com>
2017-06-02 14:06:06 -0700

    allow users to update questions in index

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,6 +11,16 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index')
