68e413bb3d2ddb95ce59aa5c6b8095eebf3183d9
Dan Jacob <dansamueljacob@gmail.com>
2017-06-02 11:05:27 -0700

    seed data in Firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  author: "Dan Jacob",
-  content: "What programming language should I learn?",
-  notes: "For a beginner"
-}, {
-  id: 2,
-  author: "John Smith",
-  content: "What is the latest trend in the tech industry?",
-  notes: "How will it affect the world?"
-]};
-
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });
